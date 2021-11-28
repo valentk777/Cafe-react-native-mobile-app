360685336e2eeaf5553832ff985c37ff8004a17d
valentk777 <valentk777@gmail.com>
2021-11-28 11:43:46 +0200

    [CMA-15] Products page with initial products list

--- CafeApp/screens/productScreen.tsx
+++ CafeApp/screens/productScreen.tsx
@@ -1,15 +1,28 @@
 import {NativeStackScreenProps} from '@react-navigation/native-stack';
 import React from 'react';
-import {View} from 'react-native';
+import {StyleSheet, View} from 'react-native';
 import {RootStackParamList} from '../App';
-import {Section} from '../components/section';
+import { Tile } from '../components/tile';
 
 type ProductScreenProps = NativeStackScreenProps<RootStackParamList, 'Product'>;
 
 export const ProductScreen = ({route}: ProductScreenProps) => {
   return (
-    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <Section title="Step One">{`Product: ${route.params.productName}`}</Section>
+    <View style={styles.container}>
+      <Tile
+        label={route.params.product.label}
+        title={route.params.product.title}
+        description={route.params.product.description}
+        image={route.params.product.image}
+      />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
--- CafeApp/screens/productsScreen.tsx
+++ CafeApp/screens/productsScreen.tsx
@@ -1,33 +1,57 @@
 import {NativeStackScreenProps} from '@react-navigation/native-stack';
 import React from 'react';
-import {View} from 'react-native';
+import {FlatList, SafeAreaView, StyleSheet, View} from 'react-native';
 import {RootStackParamList} from '../App';
-import {Button, ButtonTypes} from '../components/buttonWrapper';
+import {PressableTile} from '../components/pressableTile';
+import {globoProducts} from '../database/productsDB';
+
 type ProductsScreenProps = NativeStackScreenProps<
   RootStackParamList,
   'Products'
 >;
+
+interface ProductProps {
+  productId: string;
+  label: string;
+  title: string;
+  description: string;
+  image: string;
+}
+
 export const ProductsScreen = (props: ProductsScreenProps) => {
+
+  const renderItem = ({item}: {item: ProductProps}) => {
+    return (
+      <View>
+        <PressableTile
+          title={item.title}
+          image={item.image}
+          onPress={() => props.navigation.push('Product', {product: item})}
+        />
+      </View>
+    );
+  };
+
   return (
-    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <Button
-        type={ButtonTypes.Secondary}
-        title="Product1"
-        onPress={() =>
-          props.navigation.push('Product', {
-            productName: 'Coffee',
-          })
-        }
+    <SafeAreaView style={styles.container}>
+      <FlatList 
+        data={globoProducts}
+        renderItem={renderItem}
+        keyExtractor={item => item.productId}
+        numColumns={2}
+        showsVerticalScrollIndicator={false}
+        showsHorizontalScrollIndicator={false}
+        columnWrapperStyle={styles.columns}
       />
-      <Button
-        type={ButtonTypes.Secondary}
-        title="Product2"
-        onPress={() =>
-          props.navigation.push('Product', {
-            productName: 'Tea',
-          })
-        }
-      />
-    </View>
+    </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+  },
+  columns: {
+    justifyContent: 'space-around',
+    marginBottom: 30
+  }
+});
